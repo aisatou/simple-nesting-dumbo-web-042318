c15e371845571d97f3e075288a172c1d91af64c9
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-15 03:15:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -29,11 +29,11 @@ def alan_kay_is_known_for
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => ["Object Orientation"],
+          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
-          :known_for => ["Unix"],
+          :known_for => "Unix",
           :languages => ["C"]
         }
      }
